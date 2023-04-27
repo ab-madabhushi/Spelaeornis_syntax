3fe07dd8cc53577382ad4581ed8731177682f008
ab-madabhushi <116664143+ab-madabhushi@users.noreply.github.com>
2023-04-26 22:47:50 -0700

    Add files via upload

--- scripts_spelaeornis/signal_space.m
+++ scripts_spelaeornis/signal_space.m
@@ -17,9 +17,9 @@ scoreplotter3d(score, s_data(:,end), {'Spelaeornis caudatus', 'Spelaeornis badei
 %end
 hold off;
 %% Writing to files
-csvwrite('pc_score.csv',score,0,0)
-csvwrite('pc_loadings.csv',coeff,0,0)
-csvwrite('pc_percentage.csv',expl,0,0)
+%csvwrite('pc_score.csv',score,0,0)
+%csvwrite('pc_loadings.csv',coeff,0,0)
+%csvwrite('pc_percentage.csv',expl,0,0)
 %% 2d visualisation
 %2d visualisation
 figure();
@@ -42,4 +42,65 @@ ylim([-4 6])
 zlim([-4 4])
 %change figure limits to suit visualization
 OptionZ.FrameRate=7;OptionZ.Duration=14;OptionZ.Periodic=true;
-CaptureFigVid([-20,10;-110,10;-190,80;-290,10;-380,10],'signal_space 3d1',OptionZ);
+CaptureFigVid([-20,10;-110,10;-190,80;-290,10;-380,10],'signal_space 3d1_test',OptionZ);
+
+%% MANOVA Test
+score_3d=score(:,1:3);
+[d,p,stats]= manova1(score, sp_id)
+
+[d_par,p_par, stat_par]= manova1(song_data, s_data(:,end))
+
+
+%% Randomization Test
+% Expected Distributions
+sp_id=s_data(:,end);
+i=1
+n=1000
+avgdist_exp=NaN(n,1);
+sp=unique(sp_id);
+sp_avgdist=NaN(n,8);
+for i=1:n
+    rows=size(sp_id,1);
+    row_new=randperm(rows);
+    sp_rand=sp_id(row_new,:);
+    intsp_dist=NaN(8,8);
+    for j= 1:length(sp)-1
+        ind=sp_rand==sp(j);
+        score_sub=score(ind,:);
+        for m=j+1:length(sp)
+            ind2=sp_rand==sp(m);
+            score_sub2=score(ind2,:);
+            intsp_dist(j,m)=mean(pdist2(score_sub(:,1:3),score_sub2(:,1:3)),"all");
+        end
+        sp_avgdist(i,j)=nanmean(intsp_dist(j,:));  
+    end
+    avgdist_exp(i)=nanmean(sp_avgdist(i,:))
+
+
+end
+
+%% Observed
+j=1
+m=1
+obs_avgdist=[];
+intob_dist=NaN(8,8)
+for j= 1:length(sp)-1
+        ind=sp_id==sp(j);
+        score_sub=score(ind,:);
+        for m=j+1:length(sp)
+            ind2=sp_id==sp(m);
+            score_sub2=score(ind2,:);
+            intob_dist(j,m)=mean(pdist2(score_sub(:,1:3),score_sub2(:,1:3)),["all"]);
+        end
+        obs_avgdist=nanmean(intob_dist(j,:));
+
+end
+
+%% Z score and Z test
+
+mean_dist= mean(avgdist_exp);
+std_dist=std(avgdist_exp);
+
+z_score= (obs_avgdist-mean_dist)/std_dist;
+
+[h,p,ci,zval]=ztest(obs_avgdist,mean_dist,std_dist)
